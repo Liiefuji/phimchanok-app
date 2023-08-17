54eb84185dcb5ee22777719ab08e746ccff5752f
Liiefuji <liiefuji@gmail.com>
2023-08-17 23:59:50 +0700

    edit header

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,83 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 import engToThainLanguage from '../src/assets/engToThaiLanguage'
+import thaiToEngLanguage from '../src/assets/thaiToEngLanguage'
 
 const textarea = ref({
   height: 36,
   minRows: 5,
   curRows: 5,
-  textvalue: "",
-  textTrans: "",
-});
+  textvalue: '',
+  textTrans: ''
+})
 
-const isCopy = ref(false);
+const isCopy = ref(false)
 const textinput = ref(null)
 const textoutput = ref(null)
+const currentLanguage = ref(true)
 
 const transfromtext = (value) => {
   return value
-    .split("")
+    .split('')
     .map((item) => {
-      return engToThainLanguage[item];
+      if (currentLanguage.value) {
+        return engToThainLanguage[item]
+      } else {
+        return thaiToEngLanguage[item]
+      }
     })
-    .join("");
-};
+    .join('')
+}
+
+const onChangeLanguage = () => {
+  currentLanguage.value = !currentLanguage.value
+  textarea.value = {
+    ...textarea.value,
+    textvalue: '',
+    textTrans: '',
+    curRows: 5
+  }
+}
 
 const onChangeTextArea = () => {
-  const previousRows = textinput.value.rows;
+  const previousRows = textinput.value.rows
   //รีเซ็ตบรรทัดล่างสุดที่ไม่มีอะไรจนถึง minRows
-  textinput.value.rows = textarea.value.minRows;
+  textinput.value.rows = textarea.value.minRows
 
-  const currentRows = Math.trunc(textinput.value.scrollHeight / textarea.value.height);
+  const currentRows = Math.trunc(textinput.value.scrollHeight / textarea.value.height)
+  
+  console.log(textinput.value.scrollHeight)
+  console.log(textarea.value.height)
+  console.log(textinput.value.scrollHeight / textarea.value.height)
 
   if (currentRows === previousRows) {
-    textinput.value.rows = currentRows;
+    textinput.value.rows = currentRows
   }
 
-  let textTrans = transfromtext(textinput.value.value);
+  let textTrans = transfromtext(textinput.value.value)
 
   textarea.value = {
     ...textarea.value,
     curRows: currentRows,
     textvalue: textinput.value.value,
-    textTrans,
+    textTrans
   }
-
-};
+}
 
 const clickcopy = () => {
-  navigator.clipboard.writeText(textarea.value.textTrans);
+  navigator.clipboard.writeText(textarea.value.textTrans)
   isCopy.value = true
   setTimeout(() => {
     isCopy.value = false
-  }, 3000);
+  }, 3000)
 }
 
 const clickclear = () => {
   textarea.value = {
     ...textarea.value,
-    textvalue: "",
-    textTrans: "",
-    curRows: 5,
+    textvalue: '',
+    textTrans: '',
+    curRows: 5
   }
 }
 </script>
@@ -67,25 +86,58 @@ const clickclear = () => {
   <div class="App" :style="{ height: '100vh' }">
     <div class="navbar">
       <nav>
-        <h3>
-          <span>Forgot</span> to Change Language
-        </h3>
+        <h3><span>Forgot</span> to Change Language 2</h3>
       </nav>
     </div>
     <div class="Header">
       <h1>
-        เปลี่ยนภาษา <span>อังกฤษ</span> ให้เป็นภาษา <span>ไทย</span>
+        เปลี่ยนภาษา
+        <span @click="onChangeLanguage">
+          {{ currentLanguage ? 'อังกฤษ' : 'ไทย' }}
+        </span>
+        ให้เป็นภาษา
+        <span @click="onChangeLanguage">
+          {{ currentLanguage ? 'ไทย' : 'อังกฤษ' }}
+        </span>
       </h1>
     </div>
-    <div class="MainInputDiv " :style="{ lineHeight: textarea.height }">
-      <!-- <MdContentCopy size="1.8em" class="copyicon" :class="{ 'displaynone': textarea.textvalue }" @click="clickcopy" />
-      <MdClear :class="{ 'displaynone': textarea.textvalue }" size="2em" class="clearicon" :onClick="clickclear" /> -->
-      <textarea @input="onChangeTextArea($event)" :value="textarea.textvalue" :rows="textarea.curRows" class="textinput"
-        ref="textinput" placeholder="Enter your text" />
-      <textarea readOnly :value="textarea.textTrans" @change="onChangeTextArea" :rows="textarea.curRows" ref="textoutput"
-        class="textoutput" />
+    <div class="MainInputDiv">
+      <div class="textareaResponse">
+        <div :style="{ position: 'relative' }">
+          <font-awesome-icon
+            icon="fa-solid fa-xmark"
+            class="clearicon"
+            :class="{ displaynone: textarea.textvalue.length <= 0 }"
+            @click="clickclear"
+          />
+          <textarea
+            @input="onChangeTextArea($event)"
+            :value="textarea.textvalue"
+            :rows="textarea.curRows"
+            class="textinput textarea1"
+            ref="textinput"
+            placeholder="Enter your text"
+          />
+        </div>
+        <div :style="{ position: 'relative' }">
+          <font-awesome-icon
+            icon="fa-solid fa-copy"
+            class="copyicon"
+            :class="{ displaynone: textarea.textvalue.length <= 0 }"
+            @click="clickcopy"
+          />
+          <textarea
+            readOnly
+            :value="textarea.textTrans"
+            @change="onChangeTextArea"
+            :rows="textarea.curRows"
+            class="textoutput textarea2"
+            ref="textoutput"
+          />
+        </div>
+      </div>
     </div>
-    <div class="Snackbar" :class="{ 'show': isCopy }">
+    <div class="Snackbar" :class="{ show: isCopy }">
       <p>Copied to clipboard</p>
     </div>
   </div>
@@ -93,7 +145,7 @@ const clickclear = () => {
 
 <style scoped lang="scss">
 .navbar {
-  font-family: "Montserrat", sans-serif;
+  font-family: 'Montserrat', sans-serif;
   box-shadow: 0px 0px 10px rgb(0, 0, 0, 0.2);
   font-size: 18px;
   height: 64px;
@@ -103,6 +155,7 @@ const clickclear = () => {
   display: flex;
   align-items: center;
   justify-content: flex-start;
+  z-index: 3;
 
   nav {
     width: 90%;
@@ -158,13 +211,14 @@ const clickclear = () => {
 
   &.show {
     visibility: visible;
-    animation: fadein 0.5s forwards,
+    animation:
+      fadein 0.5s forwards,
       fadeout 0.5s forwards 2s;
   }
 }
 
 .Header {
-  font-family: "Prompt", sans-serif;
+  font-family: 'Prompt', sans-serif;
   height: auto;
   min-height: 226px;
   width: 100%;
@@ -175,7 +229,7 @@ const clickclear = () => {
   justify-content: center;
   flex-direction: column;
   background-color: #fff;
-
+  transition: all ease-in 0.3s;
   div {
     height: 42px;
     width: 70%;
@@ -187,6 +241,9 @@ const clickclear = () => {
 
   h1 {
     font-size: 40px;
+    @media (max-width: 610px) {
+      font-size: 20px;
+    }
 
     span {
       display: inline-block;
@@ -195,7 +252,7 @@ const clickclear = () => {
 
       :after {
         display: block;
-        content: "";
+        content: '';
         border-bottom: solid 3px #a30000;
         transform: scaleX(0);
         transform-origin: left;
@@ -219,10 +276,19 @@ const clickclear = () => {
   display: flex;
   align-items: flex-start;
   justify-content: center;
-
+  transition: all ease-in 0.3s;
+  .textarea1 {
+    border-right: none;
+    border-radius: 10px 0px 0px 10px;
+  }
+  .textarea2 {
+    border-radius: 0px 10px 10px 0px;
+    background-color: #f7f7f7;
+    cursor: default;
+   }
   textarea {
     max-height: 700px;
-    font-family: "Prompt", sans-serif;
+    font-family: 'Prompt', sans-serif;
     font-size: 24px;
     padding: 15px 92px 15px 15px;
     width: 100%;
@@ -232,28 +298,54 @@ const clickclear = () => {
     border-radius: 10px 0 0 10px;
     box-shadow: 2px 2px 10px rgb(0, 0, 0, 0.2);
   }
-
+  .textareaResponse {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 100%;
+    margin-bottom: 24px;
+  }
   .copyicon {
     position: absolute;
-    right: 2%;
-    top: 20px;
-    color: #919191;
-    cursor: pointer;
-
-    :hover {
-      color: #a30000;
-    }
-  }
-
-  .clearicon {
-    position: absolute;
-    left: 46%;
+    right: 16px;
     top: 16px;
     color: #919191;
     cursor: pointer;
 
     :hover {
       color: #a30000;
+    }
+    &.displaynone {
+      display: none;
+    }
+  }
+
+  .clearicon {
+    position: absolute;
+    right: 16px;
+    top: 16px;
+    color: #919191;
+    cursor: pointer;
+
+    :hover {
+      color: #a30000;
+    }
+    &.displaynone {
+      display: none;
+    }
+  }
+  @media (max-width: 1200px) {
+    width: 95%;
+  }
+  @media (max-width: 800px) {
+    textarea {
+      width: 100%;
+    }
+    .textarea1 {
+      border-radius: 10px 10px 0px 0px;
+    }
+    .textarea2 {
+      border-radius: 0px 0px 10px 10px;
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -6,9 +6,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faUserSecret, faXmark, faCopy } from '@fortawesome/free-solid-svg-icons'
+library.add(faUserSecret, faXmark, faCopy)
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 
 app.mount('#app')
